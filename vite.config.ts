ac6d576dc6df5d89435d0ee589d06d979b7d0ae8
mspiess <30656914+mspiess@users.noreply.github.com>
2026-07-18 02:57:58 +0200

    docs: add usage example to README

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,32 @@
+/// <reference types="vitest/config" />
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
 import dts from 'unplugin-dts/vite';
+import doctest from 'vite-plugin-doctest';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  plugins: [dts({
-    bundleTypes: true,
-    tsconfigPath: resolve(__dirname, 'tsconfig.lib.json'),
-  })],
+  plugins: [
+    dts({
+      bundleTypes: true,
+      tsconfigPath: resolve(__dirname, 'tsconfig.lib.json'),
+    }),
+    doctest({
+      markdown: {
+        preamble: `const { EnumSet } = await import('./src/main.ts');
+    `,
+      },
+    })],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/main.ts'),
       name: 'EnumSet',
       fileName: 'enumset',
     },
+  },
+  test: {
+    includeSource: ['./**/*.md'],
   },
 });
