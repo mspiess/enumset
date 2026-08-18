d81d72957b16e0afa9e5d5e2c8f824886e695e2b
mspiess <30656914+mspiess@users.noreply.github.com>
2026-08-18 22:48:24 +0200

    ci: add mutation testing

--- stryker.config.mjs
+++ stryker.config.mjs
@@ -4,7 +4,7 @@ const config = {
   _comment:
     'This config was generated using \'stryker init\'. Please take a look at: https://stryker-mutator.io/docs/stryker-js/configuration/ for more information.',
   packageManager: 'npm',
-  reporters: ['html', 'clear-text', 'progress'],
+  reporters: ['html', 'clear-text', 'progress', 'dashboard'],
   testRunner: 'vitest',
   testRunner_comment:
     'Take a look at https://stryker-mutator.io/docs/stryker-js/vitest-runner for information about the vitest plugin.',
